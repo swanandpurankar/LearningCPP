72dc07a21ca63582dbfaa43fcfefae0f50d3b62c
swanandpurankar <swanandpurankar@gmail.com>
2022-04-20 16:49:25 +0530

    First assignment completed - Console output and user inputs

--- 00_helloWorld/ReadMe.md
+++ 00_helloWorld/ReadMe.md
@@ -2,3 +2,5 @@
 
 First CPP project on my journey to learn C++
 Hopefully it will be continued.
+
+This file prints strings on console, accepts user inputs and prints output based on the user input.
--- 00_helloWorld/helloWorld.cpp
+++ 00_helloWorld/helloWorld.cpp
@@ -2,6 +2,19 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    int num1 = 0;
+    int num2 = 0;
+    int result = 0;
+    std::cout << "Hello, World!\t" <<  std::endl;
+    
+    std::cout << "Enter a number:  ";
+    std::cin >> num1;
+
+    std::cout << "Enter another number: ";
+    std::cin >> num2;
+
+    result = num1 + num2;
+    std::cout << "Result of addition of these two numbers is "<< result << std::endl;
+
 }
 
